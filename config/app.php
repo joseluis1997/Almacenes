0fef9ba8500184a2eb8ad091e582a9271a35f54e
joseluis1997 <joseluismercadoalarcon@gmail.com>
2020-07-26 18:01:00 -0400

    ...

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', true),
+    'debug' => env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY','base64:I6eAHukueK7YtU4JlUe41tdUZvFNnwhVtfwAmXsh8aM='),
+    'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
 
