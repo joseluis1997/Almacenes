175d8f6a5bd765046c21b7accf4c758c43298156
Alexander Willam <averapaco@gmail.com>
2021-02-21 17:45:01 -0400

    agregado el reporte

--- app/Http/Controllers/Admin/InventarioActualController.php
+++ app/Http/Controllers/Admin/InventarioActualController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Articulo;
+use DB;
 
 class InventarioActualController extends Controller
 {
@@ -13,11 +15,17 @@ class InventarioActualController extends Controller
    }
 
    public function ReporteInventarioActualPDF(){
+   	$articulos = Articulo::leftJoin(DB::raw('(SELECT COD_ARTICULO, SUM(CANTIDAD*PRECIO_UNITARIO) as total FROM DETALLE_COMPRA_STOCK GROUP BY COD_ARTICULO) as detalle'), function ($join) {
+            $join->on('detalle.COD_ARTICULO', '=', 'ARTICULO.COD_ARTICULO');
+        })->get();
+   	// dd($articulos);
 
-        // return $reporteInventarioActual = \PDF::loadView('admin.ReporteInventarioActual.RepInventarioActual');
 
-        
-        // return $reporteInventarioActual->download('admin.ReporteInventarioActual.RepInventarioActual.pdf');
-    return view('admin.ReporteInventarioActual.RepInventarioActual');
+    // return view('admin.ReporteInventarioActual.RepInventarioActual', compact('articulos'));
+
+    $reporteInventarioActual = \PDF::loadView('admin.ReporteInventarioActual.RepInventarioActual', compact('articulos'));
+
+    
+    return $reporteInventarioActual->download('admin.ReporteInventarioActual.RepInventarioActual.pdf');
    }
 }
--- resources/views/admin/ReporteInventarioActual/RepInventarioActual.blade.php
+++ resources/views/admin/ReporteInventarioActual/RepInventarioActual.blade.php
@@ -4,12 +4,143 @@
     <meta charset="utf-8">
     <title>Example 1</title>
     <link rel="stylesheet" href="style.css" media="all" />
-    <link href="{{ asset('css/ReportesStyle.css') }}" rel="stylesheet">
+    <style type="text/css">
+      .clearfix:after {
+        content: "";
+        display: table;
+        clear: both;
+      }
+
+      a {
+        color: #5D6975;
+        text-decoration: underline;
+      }
+
+      body {
+        font-family: Arial, sans-serif; 
+        font-size: 12px; 
+        font-family: Arial;
+      }
+
+      header {
+        padding: 10px 0;
+        margin-bottom: 30px;
+      }
+
+      #logo {
+        text-align: center;
+        margin-bottom: 10px;
+      }
+
+      #logo img {
+        width: 90px;
+      }
+
+      h1 {
+        border-top: 1px solid  #5D6975;
+        border-bottom: 1px solid  #5D6975;
+        color: #5D6975;
+        font-size: 2.4em;
+        line-height: 1.4em;
+        font-weight: normal;
+        text-align: center;
+        margin: 0 0 20px 0;
+        background: url(dimension.png);
+      }
+
+      #project {
+        float: left;
+      }
+
+      #project span {
+        color: #5D6975;
+        text-align: right;
+        width: 52px;
+        margin-right: 10px;
+        display: inline-block;
+        font-size: 0.8em;
+      }
+
+      #company {
+        float: right;
+        text-align: right;
+      }
+
+      #project div,
+      #company div {
+        white-space: nowrap;        
+      }
+
+      table {
+        width: 100%;
+        border-collapse: collapse;
+        border-spacing: 0;
+        margin-bottom: 20px;
+      }
+
+      table tr:nth-child(2n-1) td {
+        background: #F5F5F5;
+      }
+
+      table th,
+      table td {
+        text-align: center;
+      }
+
+      table th {
+        padding: 5px 0px;
+        color: #5D6975;
+        border-bottom: 1px solid #C1CED9;
+        white-space: nowrap;        
+        font-weight: normal;
+      }
+
+      table .inf,
+      table .desc {
+        text-align: left;
+      }
+
+      table td {
+        padding: 20px 0px;
+        text-align: right;
+      }
+
+      table td.service,
+      table td.desc {
+        vertical-align: top;
+      }
+
+      table td.unit,
+      table td.qty,
+      table td.total {
+        font-size: 1.2em;
+      }
+
+      table td.grand {
+        border-top: 1px solid #5D6975;;
+      }
+
+      #notices .notice {
+        color: #5D6975;
+        font-size: 1.2em;
+      }
+
+      footer {
+        color: #5D6975;
+        width: 100%;
+        height: 30px;
+        position: absolute;
+        bottom: 0;
+        border-top: 1px solid #C1CED9;
+        padding: 8px 0;
+        text-align: center;
+      }
+    </style>
   </head>
   <body>
     <header class="clearfix">
     <div id="logo">
-        <img src="{{ asset('images/GobernacionLogo.png') }}">
+        <img src="{{ public_path('images/GobernacionLogo.png') }}">
     </div>
         <h1><b>GOBIERNO AUTONOMO DEPARTAMENTAL DE TARIJA</b>
             UNIDAD DE ALMACENES CENTRAL<br>
@@ -29,20 +160,15 @@
           </tr>
         </thead>
         <tbody>
+          @foreach($articulos as $articulo)
           <tr>
-            <td class="inf">31110</td>
-            <td class="inf">Cafe</td>
-            <td class="inf">100</td>
-            <td class="inf">kilo</td>
-            <td class="inf">bs/100.00</td>
+            <td class="inf">{{$articulo->Partida->NRO_PARTIDA}}</td>
+            <td class="inf">{{$articulo->NOM_ARTICULO}}</td>
+            <td class="inf">{{$articulo->CANT_ACTUAL}}</td>
+            <td class="inf">{{$articulo->Medida->NOM_MEDIDA}}</td>
+            <td class="inf">bs/ {{$articulo->total}}</td>
           </tr>
-          <tr>
-            <td class="inf">31120</td>
-            <td class="inf">Cafe Instantaneo de 200 gramos</td>
-            <td class="inf">50</td>
-            <td class="inf">frasco</td>
-            <td class="inf">bs 200.00</td>
-          </tr>
+          @endforeach
         </tbody>
       </table>
       <div>
